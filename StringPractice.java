1f4f3fc370fb2de90780645bd998f8441d9ac8eb
Sarath Kumar <89997602+Skitzme@users.noreply.github.com>
2025-07-06 11:52:18 +0530

    Add files via upload

---
+++ StringPractice.java
@@ -0,0 +1,14 @@
+package StringCodes;
+
+public class StringPractice {
+
+	public static void main(String[] args) {
+		
+		//By String Litrel
+		
+		String s="Good luck!";
+		s="all the best";
+		System.out.println(s);
+	}
+
+}
